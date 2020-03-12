3f1c0f71d90bd145778bd69d020d7e1ef1eb78e2
Sang Hyun, Kim <developk@gmail.com>
2020-03-13 04:04:50 +0900

    Add files via upload

--- map.js
+++ map.js
@@ -1,12 +1,5 @@
 "use strict";
 
-var MAP_API_KEY = "D9946739-3E1E-3F78-BBEF-16009CEDDAC9";
-var AUTH_DOM = "https://myblog.dogfootk.kro.kr/";
-
-//var API_URL = "http://map.vworld.kr/js/vworldMapInit.js.do?version=2.0&apiKey=" + MAP_API_KEY + "&domain=" + AUTH_DOM;
-var API_URL = "https://map.vworld.kr/js/vworldMapInit.js.do?version=2.0&apiKey=" + MAP_API_KEY;
-//var API_URL = "http://map.vworld.kr/js/vworldMapInit.js.do?version=2.0&apiKey=" + MAP_API_KEY;
-
 var mapController;
 
 function initMap() {
@@ -24,23 +17,4 @@ function initMap() {
 	mapController = new vw.MapController(vw.MapControllerOption);
 }
 
-function pages(data, textStatus, jqxhr) {
-//	console.log('data: ', data);
-//	console.log('textStatus: ', textStatus);
-//	console.log('jqxhr: ', jqxhr);
-	console.log('지도 로드 성공!');
-	
-	initMap();
-}
-
-function mapLoadFail(jqxhr, settings, exception) {
-	console.error('jqxhr: ', jqxhr);
-	console.error('settings: ', settings);
-	console.error('exception: ', exception);
-	
-	alert('지도를 로드할 수 없습니다.');
-}
-
-$.getScript(API_URL)
-	.done(pages)
-	.fail(mapLoadFail);
+initMap();

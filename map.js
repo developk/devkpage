24a82046c722e17c68dd20fda37bf58b4f9cd38d
developk <developk@gmail.com>
2020-03-13 19:25:59 +0900

    edit

--- map.js
+++ map.js
@@ -1,20 +1,29 @@
 "use strict";
 
-var mapController;
+var vmap;
 
 function initMap() {
-	vw.MapControllerOption = {
-		container: "vmap",
-		mapMode: "2d-map",
+	vw.ol3.MapOptions = {
 		basemapType: vw.ol3.BasemapType.GRAPHIC,
+		controlsAutoArrange: true,
 		controlDensity: vw.ol3.DensityType.EMPTY,
 		interactionDensity: vw.ol3.DensityType.BASIC,
-		controlsAutoArrange: true,
 		homePosition: vw.ol3.CameraPosition,
-		initPosition: vw.ol3.CameraPosition,
+		initPosition: vw.ol3.CameraPosition
 	};
 
-	mapController = new vw.MapController(vw.MapControllerOption);
+	vmap = new vw.ol3.Map("vmap", vw.ol3.MapOptions);
+}
+
+function fnMoveZoom() {
+	zoom = vmap.getView().getZoom();
+	if (16 > zoom) {
+		vmap.getView().setZoom(14);
+	}
+};
+
+function setMode(basemapType) {
+	vmap.setBasemapType(basemapType);         
 }
 
 initMap();
